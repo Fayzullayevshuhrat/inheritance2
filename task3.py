943b0e8f0e1f2758f22bc8605573ecd7c5ea2c07
shuhrat <Fayzullayevshuhrat0904@gmail.com>
2025-02-13 10:23:05 +0500

    vazifa

--- task3.py
+++ task3.py
@@ -1,5 +1,9 @@
+import uuid
+
+
 class Product:
     def __init__(self, name, price, quantity):
+        self.__id = uuid.uuid4()
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -44,3 +48,53 @@ class Electronics(Product):
 
 f = Food("banan",23,67,"1 yil")
 print(f.info())
+
+
+
+
+
+
+class Basket:
+    def __init__(self):
+        self.__id = uuid.uuid4()
+        self.data = []
+
+    def add(self, product, price):
+        self.data.append({'product': product, 'price': price})
+        print(f"{product} ({price}) qo'shildi.")
+
+    def remove(self, product):
+        for item in self.data:
+            if item['product'] == product:
+                self.data.remove(item)
+                print(f"{product} ---1.")
+                return
+        print(f"{product} yo'q.")
+
+    def show(self):
+        if not self.data:
+            print("bo'sh.")
+        else:
+            print("bor mahsulotlari:")
+            for item in self.data:
+                print(f"- {item['product']}: {item['price']}")
+
+    def calculating(self):
+        total = sum(item['price'] for item in self.data)
+        print(f"Total price: {total}")
+
+
+basket = Basket()
+# basket.add("qulupnay", 2)
+# basket.add("kiwi", 1)
+basket.show()
+# basket.calculating()
+# basket.remove("qulupnay")
+# basket.show()
+
+
+
+
+
+
+
